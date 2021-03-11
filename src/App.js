9ad82f4158bb8041c7d094f190517e30380b566d
kian <foulkekian@ecs.vuw.ac.nz>
2021-03-11 17:10:19 +1300

    basic login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
 
+const spotify = new SpotifyWebApi();
 function App() {
-  return (
-    <div className="App">
-      React App
-    </div>
-  );
+  const [token, setToken] = useState();
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+
+    console.log("token", token);
+  }, []);
+
+  return <div className="app">{token ? <h1>Logged in</h1> : <Login />}</div>;
 }
 
 export default App;
