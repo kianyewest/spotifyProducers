7d1d830b8f10729bf7f80159ccb6dbcdb1a7530b
kian <foulkekian@ecs.vuw.ac.nz>
2021-03-23 13:58:01 +1300

    get all artist songs. Display songs grouped by artist

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 var axios = require("axios");
 
-const config = (query) =>{
+const config = (query) => {
   return {
     method: "get",
     url: `https://api.genius.com/${query}`,
@@ -16,7 +16,7 @@ const config = (query) =>{
       Cookie: "__cfduid=d938a28355c93f4c414c99724297184811615539778",
     },
   };
-}
+};
 
 var searchConfig = (query) => {
   return config(`search?q=${query}`);
@@ -25,35 +25,38 @@ var searchConfig = (query) => {
 // create a GET route
 app.get("/api/search", (req, res) => {
   // res.send({working:"its working"});
- 
-  const query = req.query.firstTrack + " by " + req.query.artistName
+
+  const query = req.query.firstTrack + " by " + req.query.artistName;
   console.log("hit API with: ", query);
   axios(searchConfig(query))
     .then(function (response) {
-      console.log(JSON.stringify(response.data))
-      const filteredForArtist = response.data.response.hits.filter((result)=>{return result.result.primary_artist.name === req.query.artistName})
-       
-      if(filteredForArtist.length>0){
+      console.log(JSON.stringify(response.data));
+      const filteredForArtist = response.data.response.hits.filter((result) => {
+        return result.result.primary_artist.name === req.query.artistName;
+      });
+
+      if (filteredForArtist.length > 0) {
         // const filteredForArtist = response.data.response.hits.filter((result)=>{return result.result.primary_artist.name === req.query.artistName})
         res.send(JSON.stringify(filteredForArtist));
-      }else{
+      } else {
         //try to get a response one more time. Just search for artist
-        console.log("trying again")
-        const nextQuery = req.query.firstTrack
-        axios(searchConfig(nextQuery))
-        .then(function (response) {
-          const filteredForArtist = response.data.response.hits.filter((result)=>{return result.result.primary_artist.name === req.query.artistName})
-          console.log("f",filteredForArtist)
+        console.log("trying again");
+        const nextQuery = req.query.firstTrack;
+        axios(searchConfig(nextQuery)).then(function (response) {
+          const filteredForArtist = response.data.response.hits.filter(
+            (result) => {
+              return result.result.primary_artist.name === req.query.artistName;
+            }
+          );
+          console.log("f", filteredForArtist);
           res.send(JSON.stringify(filteredForArtist));
-        })
+        });
       }
     })
     .catch(function (error) {
       console.log(error);
     });
 });
-
-
 
 var songConfig = (id) => {
   return config(`songs/${id}`);
@@ -62,8 +65,8 @@ var songConfig = (id) => {
 // create a GET route
 app.get("/api/song", (req, res) => {
   // res.send({working:"its working"});
- 
-  const query = req.query.id
+
+  const query = req.query.id;
   console.log("hit API with id: ", query);
   axios(songConfig(query))
     .then(function (response) {
@@ -74,46 +77,44 @@ app.get("/api/song", (req, res) => {
     });
 });
 
-
 var artistConfig = (id) => {
   return config(`artists/${id}`);
 };
 
 app.get("/api/artist", (req, res) => {
-
-  const id = req.query.artistId
+  const id = req.query.artistId;
   console.log("hit API with artist id: ", id);
   axios(artistConfig(id))
     .then(function (response) {
       // console.log("res: ",response.data)
       // console.log("hm: ",response.data.response.artist)
       // res.send(JSON.stringify({x:4,y:3}))
-       res.send(JSON.stringify(response.data.response.artist));
-      
+      res.send(JSON.stringify(response.data.response.artist));
     })
     .catch(function (error) {
       console.log(error);
     });
 });
 
-var artistSongConfig = (id) => {
-  return config(`artists/${id}/songs?per_page=50&sort=popularity`);
+var artistSongConfig = (id,pageNum) => {
+  return config(`artists/${id}/songs?per_page=50&page=${pageNum}&sort=popularity`);
 };
 
 app.get("/api/artist/songs", (req, res) => {
-  const id = req.query.artistId
+  const id = req.query.artistId;
+  var pageNum = 1;
+  if(typeof(req.query.pageNum)!=='undefined'){
+    pageNum = req.query.pageNum
+  }
   console.log("hit API with artist id: ", id);
-  axios(artistSongConfig(id))
-    .then(function (response) {
-      // console.log("res: ",response.data)
-      // console.log("hm: ",response.data.response.artist)
-      // res.send(JSON.stringify({x:4,y:3}))
-       res.send(JSON.stringify(response.data.response.songs));
-      
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  axios(artistSongConfig(id,pageNum))
+      .then(function (response) {
+        res.send(JSON.stringify(response.data.response));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  
 });
 
 app.get("/calback", (req, res) => {
--- src/ViewComponents/ViewArtist.js
+++ src/ViewComponents/ViewArtist.js
@@ -1,11 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { useRouteMatch } from "react-router-dom";
-import { Container, Spinner, ListGroup, Row, Col } from "react-bootstrap";
+import {
+  Container,
+  Spinner,
+  ListGroup,
+  Row,
+  Col,
+  Accordion,
+  Card,
+  Image,
+  Button,
+} from "react-bootstrap";
 import { Link, useLocation } from "react-router-dom";
+import _ from "lodash";
 
 function ViewArtist({ spotify }) {
   const [spotifyResults, setSpotifyResults] = useState();
-  const [geniusResults, setGeniusResults] = useState();
+  const [artistInfo, setArtistInfo] = useState();
+  const [geniusResults, setGeniusResults] = useState({
+    tracks: [],
+    nextPage: 1,
+    loading: false,
+  });
   const [noGeniusResult, setNoGeniusResult] = useState(false);
 
   const location = useLocation();
@@ -13,6 +29,36 @@ function ViewArtist({ spotify }) {
   const match = useRouteMatch();
   const spotifyArtistId = match.params.id;
   const geniusArtistId = location.state.geniusArtistId;
+
+  const getGeniusArtistTracks = () => {
+    if (geniusResults.nextPage) {
+      setGeniusResults((old) => {
+        return { ...old, loading: true };
+      });
+      fetch(
+        "/api/artist/songs?" +
+          new URLSearchParams({
+            artistId: geniusArtistId,
+            pageNum: geniusResults.nextPage,
+          })
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("songs::: ", data);
+          setGeniusResults((oldResults) => {
+            return {
+              ...oldResults,
+              tracks: [...oldResults.tracks, ...data.songs],
+              nextPage: data.next_page,
+              loading: false,
+            };
+          });
+        });
+    } else {
+      console.log("no more pages");
+    }
+  };
+
   useEffect(() => {
     // spotify.getArtist(spotifyArtistId).then(
     //   function (data) {
@@ -31,24 +77,11 @@ function ViewArtist({ spotify }) {
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log("okokokokok: ", data);
+        console.log("genius Artist info: ", data);
+        setArtistInfo(data);
       });
 
-    fetch(
-      "/api/artist/songs?" +
-        new URLSearchParams({
-          artistId: geniusArtistId,
-        })
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        console.log("songs::: ", data);
-        setGeniusResults({ tracks: data });
-        // const diffArtist =  data.filter((track) => track.primary_artist.id !== geniusArtistId)
-        // console.log("diff: ",diffArtist)
-      });
-
-    console.log("im here?");
+    getGeniusArtistTracks(1);
   }, []);
 
   const displaySpotifyResults = () => {
@@ -56,18 +89,58 @@ function ViewArtist({ spotify }) {
   };
 
   const displayGeniusResults = () => {
+    const groupArtist = _.groupBy(
+      geniusResults.tracks,
+      (track) => track.primary_artist.id
+    );
+    console.log("grouped: ", groupArtist);
     const diffArtist = geniusResults.tracks.filter(
       (track) => track.primary_artist.id !== geniusArtistId
     );
     return (
       <Container>
         <Row>
+          {artistInfo && <img src={artistInfo.header_image_url} height="128" />}
+          <h1>{artistInfo && artistInfo.name}</h1>
+          {/* <Button onClick={() => getGeniusArtistTracks()}/> */}
+          <Button
+            variant="primary"
+            onClick={() => {
+              !geniusResults.loading && getGeniusArtistTracks();
+            }}
+            disabled={geniusResults.loading}
+          >
+            {geniusResults.loading ? "Loading…" : "Click to load more"}
+          </Button>
+        </Row>
+        <Accordion>
+          {Object.keys(groupArtist).map((geniusArtistId, index) => {
+            return (
+              <Card>
+                <Accordion.Toggle as={Card.Header} eventKey={`${index}`}>
+                  {groupArtist[geniusArtistId][0].primary_artist.name} ▼
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey={`${index}`}>
+                  <Card.Body>
+                    {groupArtist[geniusArtistId].map((song) => {
+                      return song.title + ", ";
+                    })}
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            );
+          })}
+        </Accordion>
+        <Row>
           <Col sm={4}>
+            {geniusResults.tracks.length}
             <ListGroup>
               {geniusResults.tracks.map((track) => showGeniusTrack(track))}
             </ListGroup>
           </Col>
+
           <Col sm={4}>
+            {diffArtist.length}
             <ListGroup>
               {diffArtist.map((track) => showGeniusTrack(track))}
             </ListGroup>
@@ -111,4 +184,8 @@ const displayArtistsNames = (artists) => {
       </Link>
     );
   });
+};
+
+const geniusToSpotify = (tracks) => {
+  return tracks.map(() => {});
 };
