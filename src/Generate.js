6ab5bc85f9bac8fabe9c4a82cb5345776fefedad
kian <foulkekian@ecs.vuw.ac.nz>
2021-04-02 18:52:25 +1300

    display producers and list of songs

--- src/Generate.js
+++ src/Generate.js
@@ -1,13 +1,24 @@
 import React, { useState, useEffect } from "react";
-import { Spin, Row, Col } from "antd";
+import {
+  Spin,
+  Row,
+  Col,
+  Table,
+  Tag,
+  Space,
+  Card,
+  Layout,
+  Collapse,
+  Panel,
+} from "antd";
+
 import { useRouteMatch } from "react-router-dom";
-function Generate({spotify}) {
+const { Content } = Layout;
+function Generate({ spotify }) {
   const [loading, setLoading] = useState(true);
   const [displayData, setDisplayData] = useState("");
 
   const match = useRouteMatch();
-
-  console.log("match",Date.now()/1000,match);
 
   const type = (val) => {
     switch (val) {
@@ -26,7 +37,7 @@ function Generate({spotify}) {
     fetch(
       "/api/getProducers?" +
         new URLSearchParams({
-            spotifyAccessToken:spotify.getAccessToken(),
+          spotifyAccessToken: spotify.getAccessToken(),
           [type(match.params.type)]: match.params.id,
         })
     )
@@ -34,13 +45,19 @@ function Generate({spotify}) {
       .then((data) => {
         console.log("got data", data);
         // Can filter to get spotify links
-        
+
         // console.log("afterrrrr data", data.filter((song)=>{
         //   // console.log("song.media",song)
         //   return song.media && song.media.length>0 && song.media.filter((p)=>{console.log("p",p);return p.provider==="spotify"}).length>0
         // }))
         // console.log("after data", data);
-        setDisplayData(JSON.stringify(data, undefined, 2));
+
+        // data.forEach(element => {
+        //   for
+        //   element.songs.
+        // });
+
+        setDisplayData(data);
         setLoading(false);
       });
   }, []);
@@ -65,7 +82,104 @@ function Generate({spotify}) {
     );
   };
 
-  return loading ? <LoadingMessage /> : <pre>{displayData}</pre>;
+  const columns = [
+    {
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: "Artist",
+      dataIndex: "artist",
+      key: "artist",
+      render: (text) => <a>{text}</a>,
+    },
+  ];
+
+  const DisplayProducers = ({ data }) => {
+    return (
+      <Collapse>
+        {data.map((item, index) => {
+          //unable to put this in seperate component due to bug in antd code, Collapse.Panel cannot be within another component
+          const { producer, songs } = item;
+          const data = songs.map((song) => {
+            const title = song.title;
+            const artist = song.primary_artist.name;
+            return {
+              key: song.id,
+              title: title,
+              artist: artist,
+            };
+          });
+          return (
+            <Collapse.Panel
+              extra={
+                <img
+                  alt="example"
+                  src={producer.header_image_url}
+                  class="image-container"
+                />
+              }
+              headStyle={{ backgroundColor: "#fafafa" }}
+              header={producer.name}
+              key={`${index}`}
+            >
+              <Table
+                columns={columns}
+                dataSource={data}
+                pagination={false}
+                scroll={{ y: 320 }}
+              />
+            </Collapse.Panel>
+          );
+        })}
+      </Collapse>
+    );
+  };
+
+ 
+
+  const DisplayPlaylist = ({data}) => {
+    const allSongs = data.flatMap(d=>d.songs);
+    // console.log(allSongs);
+
+    const dataSource = allSongs.map((song) => {
+      const title = song.title;
+      const artist = song.primary_artist.name;
+      return {
+        key: song.id,
+        title: title,
+        artist: artist,
+      };
+    });
+
+    
+    return (
+      <Table
+        columns={columns}
+        dataSource={dataSource}
+        pagination={false}
+        scroll={{ y: 600 }}
+      />
+    );
+  };
+  return loading ? (
+    <LoadingMessage />
+  ) : (
+    <Layout className="layout">
+    <Content>
+      <Row justify="space-around">
+        <Col span={16} style={{ paddingTop: "30px" }}>
+          {displayData && <DisplayPlaylist data={displayData} />}
+        </Col>
+        <Col span={7} style={{ paddingTop: "30px" }}>
+          {displayData && <DisplayProducers data={displayData} />}
+        </Col>
+      </Row>
+    </Content>
+     </Layout>
+  );
 }
 
 export default Generate;
